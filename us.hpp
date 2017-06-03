efbddcfee999989309bbf548c73ce1ef5ad0eccf
gaoxiang <gaoxiang.ian@gmail.com>
2017-06-03 18:52:41 +0800

    rename mcontext to reg

--- us.hpp
+++ us.hpp
@@ -3,7 +3,7 @@
 // #include <iostream>
 
 namespace us {
-struct mcontext {
+struct reg {
   /*
    * The first 20 fields must match the definition of
    * sigcontext. So that we can support sigcontext
@@ -51,18 +51,7 @@ struct mcontext {
 };
 
 struct ucontext {
-  /*
-   * Keep the order of the first two fields. Also,
-   * keep them the first two fields in the structure.
-   * This way we can have a union with struct
-   * sigcontext and ucontext_t. This allows us to
-   * support them both at the same time.
-   * note: the union is not defined, though.
-   */
-  sigset_t uc_sigmask;
-  mcontext_t uc_mcontext;
-
-  struct __ucontext *uc_link;
+  reg uc_mcontext;
   stack_t uc_stack;
   int __spare__[8];
 };
