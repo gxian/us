c431a4213ad7df0458e9ffa8b4814457423efd6e
高翔 <gaoxiang.ian@gmail.com>
2018-04-06 01:01:23 +0800

    init ci

--- us.hpp
+++ us.hpp
@@ -1,16 +1,26 @@
 #pragma once
+#include "dep/json/json.hpp"
+#include "dep/pistache-promise/async.h"
 #include <cinttypes>
 
 namespace us {
 
+using json = nlohmann::json;
+
 // rpc客户端
-class Client {
-  public:
-  
+template <class Request, class Response> class Client {
+public:
+    using Promise = Pistache::Async::Promise<Response>;
+
+    // promised Connect();
+    Promise Call(Request req);
 };
 
 // rpc服务端
-class Server {};
+class Server {
+public:
+
+};
 
 // rpc通信通道定义
 class Channel {};
