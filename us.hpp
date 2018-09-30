bafa6839c088be70586428991348037653028f8a
gaoxiang <gaoxiang.ian@gmail.com>
2018-09-30 19:07:42 +0800

    调整

--- us.hpp
+++ us.hpp
@@ -6,30 +6,20 @@
 namespace us {
 
 using json = nlohmann::json;
+using Pistache::Async::Promise;
+using Handler = std::function<void()>;
 
-// rpc服务定义
-class Service {};
-
-// rpc客户端
-// 实现客户端侧的负载均衡(非对等节点的路由方式需要考虑)
-class Client {
-public:
-    template <class T>
-    using Promise = Pistache::Async::Promise<T>;
-    // promised Connect();
-    template <class Request, class Response>
-    Promise<Response> Call(Request req) {
-        json r = req;
-        return Promise<Response>();
-    }
+struct RemoteStorate {
 };
 
-// rpc服务端
-// 其他连接方式通过sidecar处理
-class Server {
-public:
-    void AddService(Service* svc);
-    void Serve();
+struct Framework {
+    virtual ~Framework() {}
+    virtual void Connect(std::string type, std::string addr,
+                         std::string pw = "", std::string user = "") = 0;
+    virtual void Listen(std::string type, int port) = 0;
+    virtual void Register(std::string path, std::string method,
+                          Handler func) = 0;
 };
 
-}
+
+}  // namespace us
